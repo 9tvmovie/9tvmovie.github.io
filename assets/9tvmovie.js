80a69f6b803b1654dbddfbdc4c69e2c1382d1936
9tvmovie <153783546+9tvmovie@users.noreply.github.com>
2023-12-14 13:00:26 -0800

    Update 9tvmovie.js

--- assets/9tvmovie.js
+++ assets/9tvmovie.js
@@ -1,1 +1,13 @@
 const apiKey = 'b2b355392c45da4dad92e5cac927bab4';
+
+/* ---------------------------------------------- /*
+        Preloader page
+/* ---------------------------------------------- */
+
+(function () {
+    $(window).on("load", function () {
+      $(".loader").fadeOut();
+      $(".page-loader").delay(1000).fadeOut("slow");
+    });
+    $(document).ready(function () {});
+  })(jQuery);
