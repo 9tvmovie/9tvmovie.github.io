d15bb2578d5e6cddc9f76d58d258b783e5b3fd29
9tvmovie <153783546+9tvmovie@users.noreply.github.com>
2023-12-21 03:46:06 +0700

    Update 9tvmovie.js

--- assets/9tvmovie.js
+++ assets/9tvmovie.js
@@ -1,5 +1,5 @@
 const apiKey = 'b2b355392c45da4dad92e5cac927bab4';
-
+const authorization = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTcwYTMwYjk1ODE5Y2IzMjA3ZTUxZjE4ZGFiNDgzNCIsInN1YiI6IjYxY2YxOTEyYWY2ZTk0MDA5ODQ3OGRkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pNjePjoP03wPNnH-lYvGa9Uqn0g6WIm1WzQXaOY3Vj8';
 /* ---------------------------------------------- /*
         Preloader page
 /* ---------------------------------------------- */
